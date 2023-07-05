5f617be7abfddbc5a87f921a1b1b6381bee9c94e
yayanrw <yayanraw@gmail.com>
2023-07-05 16:52:09 +0700

    update user_repository.ts

--- src/repositories/user_repository.ts
+++ src/repositories/user_repository.ts
@@ -7,25 +7,25 @@ class UserRepository {
     this.prisma = new PrismaClient();
   }
 
-  async createUser(userData: User): Promise<User> {
+  createUser = async (userData: User): Promise<User> => {
     return this.prisma.user.create({ data: userData });
-  }
+  };
 
-  async getUsers(): Promise<User[]> {
+  getUsers = async (): Promise<User[]> => {
     return this.prisma.user.findMany();
-  }
+  };
 
-  async getUserById(id: string): Promise<User | null> {
+  getUserById = async (id: string): Promise<User | null> => {
     return this.prisma.user.findUnique({ where: { id } });
-  }
+  };
 
-  async updateUser(id: string, userData: User): Promise<User | null> {
+  updateUser = async (id: string, userData: User): Promise<User | null> => {
     return this.prisma.user.update({ where: { id }, data: userData });
-  }
+  };
 
-  async deleteUser(id: string): Promise<User | null> {
+  deleteUser = async (id: string): Promise<User | null> => {
     return this.prisma.user.delete({ where: { id } });
-  }
+  };
 }
 
 export default UserRepository;
