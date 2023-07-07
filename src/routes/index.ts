ab43e6c9230aeb8e8a4e49eb0f5700054c8e671a
yayanrw <yayanraw@gmail.com>
2023-07-07 13:20:59 +0700

    handle not found page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,19 @@
-import express from "express";
-import UserController from "../controllers/user_controller";
+import { Router } from "express";
+import userRoutes from "./user.routes";
+import { wrapResponse } from "../core/utils/responseWrapper";
+import HttpStatusCode from "../core/enum/http-status-code";
 
-const router = express.Router();
-const userController = new UserController();
+const router = Router();
 
-router.post("/users", userController.createUser);
-router.get("/users", userController.getUsers);
-router.get("/users/:id", userController.getUserById);
-router.put("/users/:id", userController.updateUser);
-router.delete("/users/:id", userController.deleteUser);
+// Register user routes
+router.use("/users", userRoutes);
+router.get("*", function (req, res) {
+  wrapResponse({
+    res,
+    error: "NOT_FOUND",
+    message: "Route not found",
+    statusCode: HttpStatusCode.NOT_FOUND,
+  });
+});
 
 export default router;
