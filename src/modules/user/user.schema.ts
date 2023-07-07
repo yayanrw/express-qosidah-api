a650b2d7248f7d653ebeb164a126798c073ac9c9
yayanrw <yayanraw@gmail.com>
2023-07-07 15:27:15 +0700

    update user.schema.ts

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,9 +1,9 @@
 import Joi from "joi";
 
-const createUserSchema = Joi.object({
+const userSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string().email().required(),
   role: Joi.string().required().valid("Admin", "User").required(),
 });
 
-export { createUserSchema };
+export { userSchema };
