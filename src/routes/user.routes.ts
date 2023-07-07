3d150cc4524211516702e0f0e21e4f6bea6a1cae
yayanrw <yayanraw@gmail.com>
2023-07-07 14:59:43 +0700

    update user.routes.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -8,6 +8,6 @@ router.post("/", userController.createUser);
 router.get("/", userController.getUsers);
 router.get("/:id", userController.getUserById);
 router.put("/:id", userController.updateUser);
-router.delete("//:id", userController.deleteUser);
+router.delete("/:id", userController.deleteUser);
 
 export default router;
