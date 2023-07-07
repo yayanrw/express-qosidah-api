f6f8b319824e549ca16da3cb072a608bf2206f68
yayanrw <yayanraw@gmail.com>
2023-07-07 11:05:13 +0700

    feat: create exception.ts

---
+++ src/core/utils/exception.ts
@@ -0,0 +1,49 @@
+import HttpStatusCode from "../enum/http-status-code";
+
+export class NotFoundError extends Error {
+  statusCode: number;
+  errorType: string;
+
+  constructor(message: string) {
+    super(message);
+    this.errorType = "NOT_FOUND";
+    this.statusCode = HttpStatusCode.NOT_FOUND;
+    this.message = message;
+  }
+}
+
+export class ValidationError extends Error {
+  statusCode: number;
+  errorType: string;
+
+  constructor(message: string) {
+    super(message);
+    this.errorType = "VALIDATION_ERROR";
+    this.statusCode = HttpStatusCode.UNPROCESSABLE;
+    this.message = message;
+  }
+}
+
+export class BadRequestError extends Error {
+  statusCode: number;
+  errorType: string;
+
+  constructor(message: string) {
+    super(message);
+    this.errorType = "BAD_REQUEST";
+    this.statusCode = HttpStatusCode.BAD_REQUEST;
+    this.message = message;
+  }
+}
+
+export class ServerError extends Error {
+  statusCode: number;
+  errorType: string;
+
+  constructor(message: string) {
+    super(message);
+    this.errorType = "SERVER_ERROR";
+    this.statusCode = HttpStatusCode.INTERNAL_SERVER_ERROR;
+    this.message = message;
+  }
+}
