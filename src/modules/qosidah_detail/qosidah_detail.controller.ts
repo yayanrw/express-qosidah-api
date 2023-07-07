3b3177faf8f02285f11760601e510a2eb2b985d8
yayanrw <yayanraw@gmail.com>
2023-07-08 06:21:34 +0700

    feat: create qosidah_detail.controller.ts

---
+++ src/modules/qosidah_detail/qosidah_detail.controller.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import { QosidahDetail } from "@prisma/client";
+import { wrapResponse } from "../../core/utils/wrapResponse";
+import HttpStatusCode from "../../core/enum/http-status-code";
+import { wrapAsync } from "../../core/utils/wrapAsync";
+import QosidahDetailService from "./qosidah_detail.service";
+
+const qosidahDetailService = new QosidahDetailService();
+
+export default class QosidahDetailController {
+  getAll = wrapAsync(async (req: Request, res: Response) => {
+    const qosidahDetails = await qosidahDetailService.getAll();
+    wrapResponse({ res, data: qosidahDetails });
+  });
+
+  getById = wrapAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const qosidahDetail = await qosidahDetailService.getById(id);
+    wrapResponse({ res, data: qosidahDetail });
+  });
+
+  getByQosidahId = wrapAsync(async (req: Request, res: Response) => {
+    const { qosidahId } = req.params;
+    const qosidahDetails = await qosidahDetailService.getByQosudahId(qosidahId);
+    wrapResponse({ res, data: qosidahDetails });
+  });
+
+  create = wrapAsync(async (req: Request, res: Response) => {
+    const data: QosidahDetail = req.body;
+    const qosidahDetail = await qosidahDetailService.create(data);
+    wrapResponse({
+      res,
+      data: qosidahDetail,
+      message: "Qosidah Detail created",
+      statusCode: HttpStatusCode.CREATED,
+    });
+  });
+
+  update = wrapAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const updatedData: QosidahDetail = req.body;
+    const qosidahDetail = await qosidahDetailService.update(id, updatedData);
+    wrapResponse({
+      res,
+      data: qosidahDetail,
+      message: "Qosidah Detail updated",
+    });
+  });
+
+  delete = wrapAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    await qosidahDetailService.delete(id);
+    wrapResponse({ res, message: "Qosidah Detail deleted" });
+  });
+}
