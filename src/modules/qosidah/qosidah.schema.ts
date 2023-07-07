f85c436e20ea1253f94066c4553b821bf7960586
yayanrw <yayanraw@gmail.com>
2023-07-08 06:25:59 +0700

    update qosidah.schema.ts

--- src/modules/qosidah/qosidah.schema.ts
+++ src/modules/qosidah/qosidah.schema.ts
@@ -1,7 +1,9 @@
 import Joi from "joi";
 
-const keywordQosidahSchema = Joi.object({
-  keyword: Joi.string(),
+const createQosidahSchema = Joi.object({
+  title: Joi.string().required(),
+  titleLatin: Joi.string(),
+  titleTranslate: Joi.string(),
 });
 
-export { keywordQosidahSchema };
+export { createQosidahSchema };
