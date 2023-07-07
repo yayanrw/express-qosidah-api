c4871fd4e6fc71526ac2553a577ac8d11caedcd3
yayanrw <yayanraw@gmail.com>
2023-07-07 14:51:55 +0700

    update handleError.ts

--- src/core/utils/handleError.ts
+++ src/core/utils/handleError.ts
@@ -2,32 +2,31 @@
 import { wrapResponse } from "./responseWrapper";
 import HttpStatusCode from "../enum/http-status-code";
 import { Response } from "express";
-import { BadRequestError, NotFoundError } from "./exceptions";
+import { BadRequestError, NotFoundError, ValidationError } from "./exceptions";
+
+const errorMappings: { [key: string]: any } = {
+  NotFoundError: NotFoundError,
+  BadRequestError: BadRequestError,
+  ValidationError: ValidationError,
+};
 
 export function handleError(res: Response, error: any) {
-  if (error instanceof NotFoundError) {
-    return wrapResponse<null>({
-      res,
-      error: error.errorType,
-      message: error.message,
-      data: null,
-      statusCode: error.statusCode,
-    });
-  } else if (error instanceof BadRequestError) {
-    return wrapResponse<null>({
-      res,
-      error: "BAD_REQUEST",
-      message: error.message,
-      data: null,
-      statusCode: HttpStatusCode.BAD_REQUEST,
-    });
-  } else {
-    return wrapResponse<null>({
-      res,
-      error: "SERVER_ERROR",
-      message: "Internal Server Error",
-      data: null,
-      statusCode: HttpStatusCode.INTERNAL_SERVER_ERROR,
-    });
+  let errorType = "ServerError";
+  let statusCode = HttpStatusCode.INTERNAL_SERVER_ERROR;
+
+  for (const key in errorMappings) {
+    if (error instanceof errorMappings[key]) {
+      errorType = key;
+      statusCode = error.statusCode;
+      break;
+    }
   }
+
+  return wrapResponse<null>({
+    res,
+    error: errorType,
+    message: error.message,
+    data: null,
+    statusCode,
+  });
 }
