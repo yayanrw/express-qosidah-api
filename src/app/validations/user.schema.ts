ef9cd06619690966f05b28b3eb6ec649a9b7bfff
yayanrw <yayanraw@gmail.com>
2023-07-09 15:11:44 +0700

    update user.schema.ts

--- src/app/validations/user.schema.ts
+++ src/app/validations/user.schema.ts
@@ -4,6 +4,7 @@ const userSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string().email().required(),
   role: Joi.string().required().valid("Admin", "User").required(),
+  password: Joi.string().min(8),
 });
 
 export { userSchema };
