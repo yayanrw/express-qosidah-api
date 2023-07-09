5fe690f8935e10d5e56131d0adfb4158f7928788
yayanrw <yayanraw@gmail.com>
2023-07-09 14:51:27 +0700

    update user.dto.ts

--- src/app/dtos/user.dto.ts
+++ src/app/dtos/user.dto.ts
@@ -1,5 +1,19 @@
-export default interface UserDto {
+import { User } from "@prisma/client";
+
+interface UserDto {
   email: string;
   name?: string | null;
   role: string;
 }
+
+function userToUserDto(user: User): UserDto {
+  const { email, name, role } = user;
+
+  return {
+    email,
+    name: name || null,
+    role,
+  };
+}
+
+export { UserDto, userToUserDto };
