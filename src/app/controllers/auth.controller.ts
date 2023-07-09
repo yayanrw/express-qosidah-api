8741aafecb3eba9a7103a6c38009a57d3f8ec0fe
yayanrw <yayanraw@gmail.com>
2023-07-09 15:10:13 +0700

    update auth.controller.ts fix login

--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -1,44 +1,19 @@
-import { AuthenticationError } from "../../core/utils/exceptions";
 import { wrapAsync } from "../../core/utils/wrapAsync";
 import LogInDto from "../dtos/login.dto";
-import UserRepository from "../repositories/user.repository";
 import { Request, Response } from "express";
-import bcrypt from "bcrypt";
-import { createToken } from "../../core/utils/jwt";
-import { User } from "@prisma/client";
 import { wrapResponse } from "../../core/utils/wrapResponse";
+import AuthService from "../services/auth.service";
 
-const userRepository = new UserRepository();
+const authService = new AuthService();
 
 export default class AuthController {
   logIn = wrapAsync(async (req: Request, res: Response) => {
     const logInDto: LogInDto = req.body;
-
-    const user: User | null = await userRepository.getByEmail(logInDto.email);
-    if (!user) {
-      throw new AuthenticationError("Email is invalid");
-    }
-
-    const isPasswordValid = await bcrypt.compare(
-      logInDto.password,
-      user.password
-    );
-    if (!isPasswordValid) {
-      throw new AuthenticationError("Password is invalid");
-    }
-
-    const generateToken = createToken({
-      id: user.id,
-      email: user.email,
-      name: user.name,
-      role: user.role,
-    });
+    const data = await authService.logIn(logInDto);
 
     wrapResponse({
       res,
-      data: {
-        accessToken: generateToken,
-      },
+      data: data,
     });
   });
 }
