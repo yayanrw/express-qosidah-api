c48dbfd440e2cc53f17a058fb8f259ca82589661
yayanrw <yayanraw@gmail.com>
2023-07-09 12:29:48 +0700

    update auth.middleware.ts

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,8 +1,9 @@
 // authMiddleware.ts
 import { Request, Response, NextFunction } from "express";
 import { verifyToken } from "../core/utils/jwt";
-import { AuthorizationError } from "../core/utils/exceptions";
+import { AuthenticationError } from "../core/utils/exceptions";
 import { handleError } from "../core/utils/handleError";
+import User from "../core/interface/user.interface";
 
 export const authMiddleware = async (
   req: Request,
@@ -15,12 +16,13 @@ export const authMiddleware = async (
       const token = authHeader.split(" ")[1];
       const decoded = await verifyToken(token);
       if (decoded) {
+        req.user = decoded as User;
         next();
       } else {
-        throw new AuthorizationError("Invalid token");
+        throw new AuthenticationError("Invalid token");
       }
     } else {
-      throw new AuthorizationError("Authorization header missing");
+      throw new AuthenticationError("Authorization header missing");
     }
   } catch (error) {
     handleError(res, error);
