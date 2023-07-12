0e178ff677824700975fbc82a00e13938b9aea5b
yayanrw <yayanraw@gmail.com>
2023-07-12 10:56:03 +0700

    update auth.controller.ts

--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -2,9 +2,7 @@ import { wrapAsync } from "../../core/utils/wrapAsync";
 import LogInDto from "../dtos/login.dto";
 import { Request, Response } from "express";
 import { wrapResponse } from "../../core/utils/wrapResponse";
-import AuthService from "../services/auth.service";
-
-const authService = new AuthService();
+import { authService } from "../common/services";
 
 export default class AuthController {
   logIn = wrapAsync(async (req: Request, res: Response) => {
