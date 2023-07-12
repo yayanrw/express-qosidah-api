6adf808302681ad394a0236f29f4a5b925408b96
yayanrw <yayanraw@gmail.com>
2023-07-12 08:47:11 +0700

    create 1 file and update 1 file

---
+++ src/core/config/app.config.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import router from "../../routes/index.routes";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", router);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 import "dotenv/config";
-import express from "express";
-import router from "./routes";
 import dotenv from "dotenv";
+import app from "./core/config/app.config";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
-
-const app = express();
-
-app.use(express.json());
-app.use("/api", router);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
