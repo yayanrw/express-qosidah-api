905639beb4817348ead9499524cd6eaf875e2e21
yayanrw <yayanraw@gmail.com>
2023-07-12 10:56:32 +0700

    update qosidah_detail.controller.ts

--- src/app/controllers/qosidah_detail.controller.ts
+++ src/app/controllers/qosidah_detail.controller.ts
@@ -3,9 +3,7 @@ import { QosidahDetail } from "@prisma/client";
 import { wrapResponse } from "../../core/utils/wrapResponse";
 import HttpStatusCode from "../../core/enum/http-status-code";
 import { wrapAsync } from "../../core/utils/wrapAsync";
-import QosidahDetailService from "../services/qosidah_detail.service";
-
-const qosidahDetailService = new QosidahDetailService();
+import { qosidahDetailService } from "../common/services";
 
 export default class QosidahDetailController {
   getAll = wrapAsync(async (req: Request, res: Response) => {
