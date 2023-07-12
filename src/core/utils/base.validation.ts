e3ae9889aa030b276ae7519fac6b5b4ad4d6aaa5
yayanrw <yayanraw@gmail.com>
2023-07-12 08:59:23 +0700

    feat: create base.validation.ts

---
+++ src/core/utils/base.validation.ts
@@ -0,0 +1,16 @@
+import { Request } from "express";
+import { ValidationError } from "./exceptions";
+import { Schema } from "joi";
+
+const validate = (schema: Schema, req: Request) => {
+  const result = schema.validate(req, {
+    abortEarly: false,
+    allowUnknown: false,
+  });
+
+  if (result.error) {
+    throw new ValidationError(result.error.message);
+  } else {
+    return result.value;
+  }
+};
