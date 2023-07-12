3d667ea2f170bc81fa846adb955cb32b9ad0e013
yayanrw <yayanraw@gmail.com>
2023-07-12 10:38:50 +0700

    update bcrypt.helper.ts

--- src/core/utils/bcrypt.helper.ts
+++ src/core/utils/bcrypt.helper.ts
@@ -4,11 +4,11 @@ const encrypt = async (key: string): Promise<string> => {
   return await bcrypt.hash(key, 10);
 };
 
-const isPasswordValid = async (
+const isStringsValid = async (
   inputPassword: string,
   userPassword: string
 ): Promise<boolean> => {
   return await bcrypt.compare(inputPassword, userPassword);
 };
 
-export { encrypt, isPasswordValid };
+export { encrypt, isStringsValid };
