ebc0060db9de0dd0c32a558279f1aab79cdd0206
yayanrw <yayanraw@gmail.com>
2023-07-12 11:25:13 +0700

    update pagination_params.validation.ts

--- src/app/validations/pagination_params.validation.ts
+++ src/app/validations/pagination_params.validation.ts
@@ -3,8 +3,8 @@ import Joi from "joi";
 const paginationParamsValidation = Joi.object({
   page: Joi.string().required(),
   pageSize: Joi.string().required(),
-  orderBy: Joi.string().default("id"),
-  orderDirection: Joi.string().default("asc"),
+  orderBy: Joi.string().allow(null).empty("").default("id"),
+  orderDirection: Joi.string().allow(null).empty("").default("asc"),
   filter: Joi.object().optional(),
 });
 
