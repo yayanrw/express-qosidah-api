0eb815ce0bc96218e5d0684c73414737677617f2
yayanrw <yayanraw@gmail.com>
2023-07-12 11:25:10 +0700

    update logger.config.ts

--- src/core/config/logger.config.ts
+++ src/core/config/logger.config.ts
@@ -1,9 +1,29 @@
 import winston from "winston";
+import util from "util";
+
+const colorizer = winston.format.colorize({
+  colors: {
+    error: "red",
+    warn: "yellow",
+    info: "green",
+    debug: "cyan",
+  },
+});
+
+const logFormat = winston.format.printf(({ level, message, timestamp }) => {
+  const coloredLevel = colorizer.colorize(level, level.toUpperCase());
+  const beautifiedMessage = util.inspect(message, { colors: true });
+  return `${timestamp} ${coloredLevel}: ${beautifiedMessage}`;
+});
 
 const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  transports: [new winston.transports.Console({})],
+  level: "debug",
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.splat(),
+    logFormat
+  ),
+  transports: [new winston.transports.Console()],
 });
 
 export default logger;
