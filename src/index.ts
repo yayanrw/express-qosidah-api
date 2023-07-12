3460843604561b7baa709205a06abb2a3aad28ec
yayanrw <yayanraw@gmail.com>
2023-07-12 11:03:18 +0700

    update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
 import dotenv from "dotenv";
 import app from "./core/config/app.config";
+import logger from "./core/config/logger.config";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+  logger.info(`App listening on port ${port}`);
 });
