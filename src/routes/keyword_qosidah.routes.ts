c66adc70abc14c23c8e18c009273f354e2c32747
yayanrw <yayanraw@gmail.com>
2023-07-13 15:59:56 +0700

    update keyword_qosidah.routes.ts

--- src/routes/keyword_qosidah.routes.ts
+++ src/routes/keyword_qosidah.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import KeywordQosidahController from "../app/controllers/keyword_qosidah.controller";
 import { adminMiddleware } from "../middlewares/admin.middleware";
 import { authMiddleware } from "../middlewares/auth.middleware";
+import { cacheMiddleware } from "../middlewares/cache.middleware";
 
 const router = Router();
 const keywordQosidahController = new KeywordQosidahController();
@@ -11,7 +12,11 @@ router.post(
   [authMiddleware, adminMiddleware],
   keywordQosidahController.create
 );
-router.get("/", [authMiddleware], keywordQosidahController.getAll);
+router.get(
+  "/",
+  [authMiddleware, cacheMiddleware],
+  keywordQosidahController.getAll
+);
 router.get("/:id", [authMiddleware], keywordQosidahController.getById);
 router.put(
   "/:id",
