3e3572c55c753a329d5fc6a05c65641084a49395
yayanrw <yayanraw@gmail.com>
2023-07-13 15:17:24 +0700

    update redis.config.ts

--- src/core/config/redis.config.ts
+++ src/core/config/redis.config.ts
@@ -2,6 +2,7 @@ import { createClient } from "redis";
 import logger from "./logger.config";
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_EXPIRE_TIMEOUT = Number(process.env.REDIS_EXPIRE_TIMEOUT) || 86400;
 
 const redisClient = createClient({
   url: REDIS_URL,
@@ -19,4 +20,4 @@ const redisConnect = async () => {
   await redisClient.connect();
 };
 
-export { redisClient, redisConnect };
+export { redisClient, redisConnect, REDIS_EXPIRE_TIMEOUT };
