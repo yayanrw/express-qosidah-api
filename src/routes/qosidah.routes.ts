16003e54c764fc859f8dfce615c8c59382bcae5b
yayanrw <yayanraw@gmail.com>
2023-07-13 14:47:13 +0700

    update qosidah.routes.ts

--- src/routes/qosidah.routes.ts
+++ src/routes/qosidah.routes.ts
@@ -2,13 +2,18 @@ import { Router } from "express";
 import QosidahController from "../app/controllers/qosidah.controller";
 import { adminMiddleware } from "../middlewares/admin.middleware";
 import { authMiddleware } from "../middlewares/auth.middleware";
+import { cacheMiddleware } from "../middlewares/cache.middleware";
 
 const router = Router();
 const qosidahController = new QosidahController();
 
 router.post("/", [authMiddleware, adminMiddleware], qosidahController.create);
 router.get("/", [authMiddleware], qosidahController.populate);
-router.get("/:id", [authMiddleware], qosidahController.getById);
+router.get(
+  "/:id",
+  [authMiddleware, cacheMiddleware],
+  qosidahController.getById
+);
 router.put("/:id", [authMiddleware, adminMiddleware], qosidahController.update);
 router.put(
   "/:id/published",
