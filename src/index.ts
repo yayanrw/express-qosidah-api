b3ac833f6bf84c9c4904f1ba9646c94609f963dd
yayanrw <yayanraw@gmail.com>
2023-07-13 14:48:09 +0700

    update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import "dotenv/config";
 import dotenv from "dotenv";
 import app from "./core/config/app.config";
 import logger from "./core/config/logger.config";
+import { redisConnect } from "./core/config/redis.config";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
+
+redisConnect();
 
 app.listen(port, () => {
   logger.info(`App listening on port ${port}`);
