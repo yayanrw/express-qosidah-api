70991163d846c7ba42eb8d43b1bdfaa1471e9823
yayanrw <yayanraw@gmail.com>
2023-10-14 13:40:47 +0700

    update redis.config.ts

--- src/core/config/redis.config.ts
+++ src/core/config/redis.config.ts
@@ -1,18 +1,24 @@
-import { createClient } from "redis";
-import logger from "./logger.config";
+import { createClient } from 'redis';
+import logger from './logger.config';
 
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_HOST = process.env.REDIS_HOST || 'redis://localhost:6379';
+const REDIS_PORT = Number(process.env.REDIS_PORT) || 18991;
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD || 'password';
 const REDIS_EXPIRE_TIMEOUT = Number(process.env.REDIS_EXPIRE_TIMEOUT) || 86400;
 
 const redisClient = createClient({
-  url: REDIS_URL,
+  password: REDIS_PASSWORD,
+  socket: {
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+  },
 });
 
-redisClient.on("connect", () => {
-  logger.info("Redis client connected");
+redisClient.on('connect', () => {
+  logger.info('Redis client connected');
 });
 
-redisClient.on("error", (err: Error) => {
+redisClient.on('error', (err: Error) => {
   logger.error(`Redis client error: ${err}`);
 });
 
